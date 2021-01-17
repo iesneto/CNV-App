cea4cbdd9dd002e6a2f34ffd49f501a737879b50
iesneto <iesneto@gmail.com>
2021-01-17 19:15:33 -0300

    Navegação entre Paineis
    1.1 Primeiro fluxo de navegação entre os paineis do App
    
    1.2 Adicionada uma Scroll View que conterá as cartas das necessidades que podem ser escolhidas
    
    1.3 Cartas ja contém um script que as torna interativas por click
    
    1.4 Painel de Boas Vindas fica ativo por um tempo definido por um parametro

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject objectPanels;
+    [SerializeField] private List<GameObject> listPanels;
+    [SerializeField] private GameObject currentPanel;
+    [SerializeField] private int panelIndex;
+    [System.Serializable]
+    private struct ParametrosUIStruct
+    {
+        public float boasVindasPainelTime;
+    }
+    [SerializeField] private ParametrosUIStruct ParametrosUI;
+
+    private void Awake()
+    {
+        panelIndex = 0;
+        
+        foreach(Transform child in objectPanels.transform)
+        {
+            child.gameObject.SetActive(false);
+            listPanels.Add(child.gameObject);
+        }
+
+        currentPanel = listPanels[panelIndex];
+        currentPanel.SetActive(true);
+        StartCoroutine(BemVindoCoroutine());
+    }
+
+    public void NextPanel()
+    {
+        // desabilita painel atual
+        currentPanel.SetActive(false);
+        // aponta para o proximo
+        panelIndex++;
+        // pega o proximo painel
+        currentPanel = listPanels[panelIndex];
+        // habilita o proximo painel
+        currentPanel.SetActive(true);
+    }
+
+    public void PreviousPanel()
+    {
+        // desabilita painel atual
+        currentPanel.SetActive(false);
+        // aponta para o anterior
+        panelIndex--;
+        // pega o painel anterior
+        currentPanel = listPanels[panelIndex];
+        // habilita o painel anterior
+        currentPanel.SetActive(true);
+    }
+
+    IEnumerator BemVindoCoroutine()
+    {
+        yield return new WaitForSeconds(ParametrosUI.boasVindasPainelTime);
+        NextPanel();
+    }
+}
