bcd4c7e818a126b55c7870e8cbc29b8609839b85
iesneto <iesneto@gmail.com>
2021-01-20 22:12:50 -0300

    ScriptableObjects
    Adicionado as Cartas dos Sentimentos e das Necessidades

--- Assets/Scripts/ScriptableObjects/CartaScrObj.cs
+++ Assets/Scripts/ScriptableObjects/CartaScrObj.cs
@@ -3,6 +3,9 @@
 [CreateAssetMenu(fileName = "NovoDadosCarta", menuName = "ScriptableObjects/DadosCartaAsset", order = 1)]
 public class CartaScrObj : ScriptableObject
 {
+    public enum CartaTipo { NECESSIDADE, SENTIMENTO}
+
+    public CartaTipo tipo;
     public string nome;
     public Sprite imagem;
     public string descricao;
